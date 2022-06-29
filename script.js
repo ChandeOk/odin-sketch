93ab8a9acc1d38429a6afb42ed2c225bba6363c7
chandeok <rajivqcnstg@gmail.com>
2022-06-29 19:01:14 +0300

    its working

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 'use strict';
 
-let squares = 32;
+let numberOfSquares = 256;
 
 const container = document.querySelector('.container');
+const input = document.querySelector('form');
+console.log(input);
+const renderGrid = function () {
+  container.innerHTML = '';
+  container.setAttribute(
+    'style',
+    `grid-template-columns: repeat(${Math.sqrt(numberOfSquares)}, 1fr)`
+  );
+  for (let i = 0; i < numberOfSquares; i++) {
+    const div = document.createElement('div');
+    div.classList.add('square');
+    container.appendChild(div);
+  }
+};
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement('div');
-  div.classList.add('square');
-  container.appendChild(div);
-}
+renderGrid();
 
 container.addEventListener('mouseover', function (event) {
   const hoveredSquare = event.target.closest('.square');
   if (!hoveredSquare) return;
   hoveredSquare.style.backgroundColor = 'black';
+});
+
+input.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const value = +this.querySelector('input').value;
+  if (value > 100) return;
+  numberOfSquares = value * value;
+  renderGrid();
 });
